1ab544a42bb7ae55b342c46ff739e8f5a7a7109f
咩屁啊 <970695436@qq.com>
2020-07-13 23:46:21 +0800

    <Feat>:<去掉 utils> 【原因分析】
    【解决方案】
    
    【影响模块】
    
    【测试建议】

--- src/renderer/database/data-store.js
+++ src/renderer/database/data-store.js
@@ -1,11 +1,26 @@
-import Datastore from 'nedb'
+import Nedb from 'nedb'
 import path from 'path'
 import { remote } from 'electron'
 
 // TODO:文件名称修改
-export default new Datastore({
-  autoload: true,
-  filename: path.join(remote.app.getPath('userData'), '/electron/data.db')
-})
-
-// TODO:考虑多数据库
+export default class Datastore extends Nedb {
+  /**
+   * description: 构造函数
+   *
+   * @param {Object} db 数据库参数
+   * @param {String} [db.remotePath = 'userData'] 存储位置
+   * @param {String} db.dbName 数据库名称
+   *
+   * created by zhouzt on 2020-07-11
+   *
+   */
+  constructor ({
+    remotePath = 'userData',
+    dbName
+  }) {
+    super({
+      autoload: true,
+      filename: path.join(remote.app.getPath(remotePath), `/electron/${dbName}.db`)
+    })
+  }
+}
